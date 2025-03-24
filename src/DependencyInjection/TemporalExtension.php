2503b0f3e9885c830bf9564265a43ae2e8e91caf
Grégory Planchat <gregory@gyroscops.com>
2025-03-24 14:57:38 +0100

    Added testing functionality Removed the WorkerFactory class, now useless Backported features from version 1.2

--- src/DependencyInjection/TemporalExtension.php
+++ src/DependencyInjection/TemporalExtension.php
@@ -4,96 +4,314 @@ declare(strict_types=1);
 
 namespace Highcore\TemporalBundle\DependencyInjection;
 
-use Highcore\TemporalBundle\WorkerFactory;
 use Highcore\TemporalBundle\WorkflowClientFactory;
 use Highcore\TemporalBundle\WorkflowClientFactoryInterface;
 use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\Alias;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Definition;
+use Symfony\Component\DependencyInjection\Exception\InvalidArgumentException;
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\PhpFileLoader;
 use Symfony\Component\DependencyInjection\Reference;
-use Temporal\Client\ClientOptions;
 use Temporal\Client\WorkflowClient;
+use Temporal\DataConverter\DataConverter;
 use Temporal\DataConverter\DataConverterInterface;
+use Temporal\Worker\ActivityInvocationCache\ActivityInvocationCacheInterface;
+use Temporal\Worker\ActivityInvocationCache\InMemoryActivityInvocationCache;
+use Temporal\Worker\ActivityInvocationCache\RoadRunnerActivityInvocationCache;
 use Temporal\Worker\WorkerFactoryInterface;
-use Temporal\WorkerFactory as TemporalWorkerFactory;
+use Temporal\WorkerFactory as WorkerFactory;
+use Temporal\Testing\WorkerFactory as TestingWorkerFactory;
 use function Symfony\Component\DependencyInjection\Loader\Configurator\service;
 
 final class TemporalExtension extends Extension
 {
     const TEMPORAL_OPTIONS_SERVICE_ID = 'temporal.workflow_client.options';
 
+    private function isTestingEnabled(array $options): bool
+    {
+        return $config['worker']['testing']['enabled'] ?? false;
+    }
+
+    private function extractWorkerQueue(array $config, string $default = 'default'): string
+    {
+        return $config['worker']['queue'] ?? $default;
+    }
+
+    private function extractTemporalAddress(array $config, string $default = 'localhost:7233'): string
+    {
+        return $config['address'] ?? $default;
+    }
+
+    private function extractTemporalNamespace(array $config, string $default = 'default'): string
+    {
+        return $config['namespace'] ?? $default;
+    }
+
+    private function extractWorkflowClientOptions(array $config): array
+    {
+        return $config['workflow_client']['options'] ?? [];
+    }
+
+    private function extractWorkflowClientFactoryClass(array $config, string $default = WorkflowClientFactory::class): string
+    {
+        $class = $config['workflow_client']['factory'] ?? $default;
+
+        if (!is_a($class, WorkflowClientFactoryInterface::class, true)) {
+            throw new InvalidArgumentException(\sprintf(
+                'Class "%s" should implement "%s" interface.',
+                $class,
+                WorkflowClientFactoryInterface::class,
+            ));
+        }
+
+        return $class;
+    }
+
+    private function extractDataConverterClasses(array $config): array
+    {
+        return $config['worker']['data_converter']['converters'] ?? [];
+    }
+
+    private function extractDataConverterFacadeClass(array $config, string $default = DataConverter::class): string
+    {
+        return $config['worker']['data_converter']['class'] ?? $default;
+    }
+
+    private function extractWorkerFactoryClass(array $config, string $default = WorkerFactory::class): string
+    {
+        return $config['worker']['factory'] ?? $default;
+    }
+
+    private function extractActivityInvocationCacheClass(array $config, string $default = InMemoryActivityInvocationCache::class): string
+    {
+        return $config['worker']['testing']['activity_invocation_cache'] ?? $default;
+    }
+
+    private function registerWorkerQueueParameter(
+        array $config,
+        ContainerBuilder $container,
+        string $defaultQueue = 'default',
+    ): void {
+        $container->setParameter(
+            'temporal.worker.queue',
+            $this->extractWorkerQueue($config, $defaultQueue),
+        );
+    }
+
+    private function registerTemporalAddressParameter(
+        array $config,
+        ContainerBuilder $container,
+    ): void {
+        $container->setParameter(
+            'temporal.address',
+            $this->extractTemporalAddress($config),
+        );
+    }
+
+    private function registerTemporalNamespaceParameter(
+        array $config,
+        ContainerBuilder $container,
+        string $defaultNamespace = 'default',
+    ): void {
+        $container->setParameter(
+            'temporal.namespace',
+            $this->extractTemporalNamespace($config, $defaultNamespace),
+        );
+    }
+
+    private function registerDataConverterService(
+        array $config,
+        ContainerBuilder $container,
+        string $defaultDataConverterFacadeClass = DataConverter::class,
+    ): Definition {
+        $dataConverterFacadeClass = $this->extractDataConverterFacadeClass($config, $defaultDataConverterFacadeClass);
+        if ($container->hasDefinition($dataConverterFacadeClass)) {
+            return $container->getDefinition($dataConverterFacadeClass);
+        }
+
+        return $container->register($dataConverterFacadeClass)
+            ->setArguments(array_map(function ($dataConverterDelegateClass) use ($container): Reference {
+                if (class_exists($dataConverterDelegateClass) && !$container->hasDefinition($dataConverterDelegateClass)) {
+                    $container->register($dataConverterDelegateClass)
+                        ->setAutoconfigured(true)
+                        ->setAutowired(true)
+                    ;
+                }
+
+                return new Reference($dataConverterDelegateClass);
+            }, $this->extractDataConverterClasses($config)));
+    }
+
+    private function registerDataConverterAlias(
+        array $config,
+        ContainerBuilder $container,
+        string $defaultDataConverterFacadeClass = DataConverter::class,
+    ): Alias {
+        if ($container->hasAlias(DataConverterInterface::class)) {
+            return $container->getAlias(DataConverterInterface::class);
+        }
+
+        return $container->setAlias(DataConverterInterface::class, $this->extractDataConverterFacadeClass($config, $defaultDataConverterFacadeClass));
+    }
+
+    private function registerWorkerFactoryService(
+        array $config,
+        ContainerBuilder $container,
+        string $defaultWorkerFactoryClass = WorkerFactory::class,
+        string $defaultDataConverterFacadeClass = DataConverter::class,
+        string $defaultActivityInvocationCacheClass = InMemoryActivityInvocationCache::class,
+    ): Definition {
+        $workerFactoryClass = $this->extractWorkerFactoryClass($config, $defaultWorkerFactoryClass);
+        if ($container->hasDefinition($workerFactoryClass)) {
+            return $container->getDefinition($workerFactoryClass);
+        }
+
+        if ($this->isTestingEnabled($config)) {
+            return $container->register($workerFactoryClass)
+                ->setPublic(true)
+                ->setAutowired(true)
+                ->setAutoconfigured(true)
+                ->setFactory("{$workerFactoryClass}::create")
+                ->setArgument('$converter', new Reference($this->extractDataConverterFacadeClass($config, $defaultDataConverterFacadeClass)))
+                ->setArgument('$activityCache', new Reference($this->extractActivityInvocationCacheClass($config, $defaultActivityInvocationCacheClass)))
+            ;
+        }
+
+        return $container->register($workerFactoryClass)
+            ->setPublic(true)
+            ->setAutowired(true)
+            ->setAutoconfigured(true)
+            ->setFactory("{$workerFactoryClass}::create")
+            ->setArgument('$converter', new Reference($this->extractDataConverterFacadeClass($config, $defaultDataConverterFacadeClass)))
+        ;
+    }
+
+    private function registerWorkerFactoryAlias(
+        array $config,
+        ContainerBuilder $container,
+        string $defaultWorkerFactoryClass = WorkerFactory::class,
+    ): Alias {
+        $workerFactoryClass = $this->extractWorkerFactoryClass($config, $defaultWorkerFactoryClass);
+        if ($container->hasDefinition($workerFactoryClass)) {
+            return $container->setAlias(WorkerFactoryInterface::class, $workerFactoryClass);
+        }
+
+        if ($this->isTestingEnabled($config)) {
+            return $container->setAlias(WorkerFactoryInterface::class, TestingWorkerFactory::class);
+        }
+
+        return $container->setAlias(WorkerFactoryInterface::class, WorkerFactory::class);
+    }
+
+    private function registerActivityInvocationCacheService(
+        array $config,
+        ContainerBuilder $container,
+        string $defaultActivityInvocationCacheClass = InMemoryActivityInvocationCache::class,
+        string $defaultDataConverterFacadeClass = DataConverter::class,
+    ): Definition {
+        return match ($this->extractActivityInvocationCacheClass($config, $defaultActivityInvocationCacheClass)) {
+            RoadRunnerActivityInvocationCache::class => $this->registerRoadRunnerActivityInvocationCacheService($config, $container, $defaultDataConverterFacadeClass),
+            InMemoryActivityInvocationCache::class => $this->registerInMemoryActivityInvocationCacheService($config, $container, $defaultDataConverterFacadeClass),
+            default => $container->getDefinition($this->extractActivityInvocationCacheClass($config, $defaultActivityInvocationCacheClass)),
+        };
+    }
+
+    private function registerRoadRunnerActivityInvocationCacheService(
+        array $config,
+        ContainerBuilder $container,
+        string $defaultDataConverterFacadeClass = DataConverter::class,
+    ): Definition {
+        if ($container->hasDefinition(RoadRunnerActivityInvocationCache::class)) {
+            return $container->getDefinition(RoadRunnerActivityInvocationCache::class);
+        }
+
+        return $container->register(RoadRunnerActivityInvocationCache::class)
+            ->setFactory(sprintf('%s::create', RoadRunnerActivityInvocationCache::class))
+            ->addArgument('$dataConverter', new Reference($this->extractDataConverterFacadeClass($config, $defaultDataConverterFacadeClass)))
+        ;
+    }
+
+    private function registerInMemoryActivityInvocationCacheService(
+        array $config,
+        ContainerBuilder $container,
+        string $defaultDataConverterFacadeClass = DataConverter::class,
+    ): Definition {
+        if ($container->hasDefinition(InMemoryActivityInvocationCache::class)) {
+            return $container->getDefinition(InMemoryActivityInvocationCache::class);
+        }
+
+        return $container->register(InMemoryActivityInvocationCache::class)
+            ->setFactory(sprintf('%s::create', InMemoryActivityInvocationCache::class))
+            ->addArgument('$dataConverter', new Reference($this->extractDataConverterFacadeClass($config, $defaultDataConverterFacadeClass)))
+        ;
+    }
+
+    private function registerActivityInvocationCacheAlias(
+        array $config,
+        ContainerBuilder $container,
+        string $defaultActivityInvocationCacheClass = InMemoryActivityInvocationCache::class,
+    ): Alias {
+        $activityInvocationCacheClass = $this->extractActivityInvocationCacheClass($config, $defaultActivityInvocationCacheClass);
+
+        return $container->setAlias(ActivityInvocationCacheInterface::class, new Alias($activityInvocationCacheClass));
+    }
+
+    private function registerWorkflowClientFactoryService(
+        array $config,
+        ContainerBuilder $container,
+        string $defaultWorkflowClientFactoryClass = WorkflowClientFactory::class,
+        string $defaultDataConverterFacadeClass = DataConverter::class,
+    ): Definition {
+        $workflowClientFactoryClass = $this->extractWorkflowClientFactoryClass($config, $defaultWorkflowClientFactoryClass);
+
+        if ($container->hasDefinition($workflowClientFactoryClass)) {
+            return $container->getDefinition($workflowClientFactoryClass);
+        }
+
+        return $container->register($workflowClientFactoryClass)
+            ->setPublic(true)
+            ->setAutowired(true)
+            ->setAutoconfigured(true)
+            ->addMethodCall('setDataConverter', [new Reference($this->extractDataConverterFacadeClass($config, $defaultDataConverterFacadeClass))])
+            ->addMethodCall('setAddress', [$this->extractTemporalAddress($config)])
+            ->addMethodCall('setOptions', [$this->extractWorkflowClientOptions($config)])
+        ;
+    }
+
+    private function registerWorkflowClientService(
+        array $config,
+        ContainerBuilder $container,
+        string $defaultWorkflowClientFactoryClass = WorkflowClientFactory::class,
+    ): Definition {
+        return $container->register(WorkflowClient::class)
+                ->setFactory(new Reference($this->extractWorkflowClientFactoryClass($config, $defaultWorkflowClientFactoryClass)));
+    }
+
     public function load(array $configs, ContainerBuilder $container): void
     {
         $config = $this->processConfiguration(new Configuration(), $configs);
 
         $options = $config['workflow_client']['options'] ?? [];
-        $workflowClientFactoryClass = $config['workflow_client']['factory'] ?? WorkflowClientFactory::class;
-        $dataConverterConverters = $config['worker']['data_converter']['converters'];
-        $workerFactory = $config['worker']['factory'] ?? WorkerFactory::class;
-        $dataConverterClass = $config['worker']['data_converter']['class'];
-        $queue = $config['worker']['queue'] ?? 'default';
-        $namespace = $options['namespace'] ?? 'default';
-        $temporalRpcAddress = $config['address'];
 
-        $container->setParameter('temporal.worker.queue', $queue);
-        $container->setParameter('temporal.address', $temporalRpcAddress);
-        $container->setParameter('temporal.namespace', $namespace);
+        $this->registerWorkerQueueParameter($config, $container);
+        $this->registerTemporalAddressParameter($config, $container);
+        $this->registerTemporalNamespaceParameter($config, $container);
 
-        if (!is_a($workflowClientFactoryClass, WorkflowClientFactoryInterface::class, true)) {
-            throw new \RuntimeException(\sprintf('Class "%s" should implemenets "%s" interface.', $workflowClientFactoryClass, WorkflowClientFactoryInterface::class));
+        $this->registerDataConverterService($config, $container);
+        $this->registerDataConverterAlias($config, $container);
+        $this->registerWorkerFactoryService($config, $container);
+        $this->registerWorkerFactoryAlias($config, $container);
+        $this->registerWorkflowClientFactoryService($config, $container);
+        $this->registerWorkflowClientService($config, $container);
+
+        if ($this->isTestingEnabled($options)) {
+            $this->registerActivityInvocationCacheService($config, $container);
+            $this->registerActivityInvocationCacheAlias($config, $container);
         }
-
-        [$workerFactoryClass] = explode('::', $workerFactory);
-        if (!$container->hasDefinition($workerFactoryClass)) {
-            $container->setDefinition($workerFactoryClass, (new Definition($workerFactoryClass))
-                ->setPublic(true)->setAutowired(true)->setAutoconfigured(true)
-                ->setArgument('$dataConverter', new Reference($dataConverterClass))
-            );
-        }
-
-        $dataConverterDefinition = $container->register($dataConverterClass, $dataConverterClass);
-        $serviceConverters = [];
-        foreach ($dataConverterConverters as $converterId) {
-            if (class_exists($converterId) && !$container->hasDefinition($converterId)) {
-                $container->setDefinition($converterId, (new Definition($converterId))
-                    ->setAutoconfigured(true)
-                    ->setAutowired(true)
-                    ->setPublic(true)
-                );
-            }
-
-            $serviceConverters[] = new Reference($converterId);
-        }
-        $dataConverterDefinition->setArguments($serviceConverters);
-
-        if (!$container->hasAlias(DataConverterInterface::class)) {
-            $container->setAlias(DataConverterInterface::class, $dataConverterClass);
-        }
-
-        if (!$container->hasDefinition($workflowClientFactoryClass)) {
-            $container->setDefinition($workflowClientFactoryClass, (new Definition($workflowClientFactoryClass))
-                ->setPublic(true)->setAutowired(true)->setAutoconfigured(true)
-                ->addMethodCall('setDataConverter', [new Reference($dataConverterClass)])
-                ->addMethodCall('setAddress', [$temporalRpcAddress])
-                ->addMethodCall('setOptions', [$options])
-            );
-        }
-
-        $workerFactory = str_contains('::', $workerFactory) ? $workerFactory : new Reference($workerFactoryClass);
-        $container->register(TemporalWorkerFactory::class, TemporalWorkerFactory::class)
-            ->setFactory($workerFactory);
-        $container->register(WorkflowClient::class, WorkflowClient::class)
-            ->setFactory([new Reference($workflowClientFactoryClass), '__invoke']);
-
-        $container->setAlias(WorkerFactoryInterface::class, TemporalWorkerFactory::class);
 
         $loader = new PhpFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.php');
-
-        if (!$container->hasDefinition($dataConverterClass)) {
-            throw new \LogicException('Data Converter Class "%s" should be registered.');
-        }
     }
 }
--- src/Registry/ActivityRegistry.php
+++ src/Registry/ActivityRegistry.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Highcore\TemporalBundle\Registry;
 
 use Temporal\Activity\ActivityInterface;
+use Temporal\Activity\LocalActivityInterface;
 
 final class ActivityRegistry
 {
@@ -13,12 +14,11 @@ final class ActivityRegistry
     public function add($activity): void
     {
         $reflection = new \ReflectionObject($activity);
-        $attributes = $this->getAllAttributes($reflection);
 
-        if (!\in_array(ActivityInterface::class, $attributes, true)) {
+        if (!$this->isActivity($reflection)) {
             throw new \LogicException(\sprintf(
-                'Class "%s" does not have "%s" attribute.',
-                $activity::class, ActivityInterface::class));
+                'Class "%s" does not have "%s" or "%s" attribute.',
+                $activity::class, ActivityInterface::class, LocalActivityInterface::class));
         }
 
         $this->activities[] = $activity;
@@ -29,18 +29,22 @@ final class ActivityRegistry
         return $this->activities;
     }
 
-    private function getAllAttributes(\ReflectionObject $reflection): array
+    private function isActivity(\ReflectionObject $reflection): bool
     {
-        $attributes = [];
-        $attributes[] = $reflection->getAttributes();
-
-        foreach ($reflection->getInterfaces() as $interface) {
-            $attributes[] = $interface->getAttributes();
+        if (\count($reflection->getAttributes(ActivityInterface::class)) >= 1
+            || \count($reflection->getAttributes(LocalActivityInterface::class)) >= 1
+        ) {
+            return true;
         }
 
+        foreach ($reflection->getInterfaces() as $interface) {
+            if (\count($interface->getAttributes(ActivityInterface::class)) >= 1
+                || \count($interface->getAttributes(LocalActivityInterface::class)) >= 1
+            ) {
+                return true;
+            }
+        }
 
-        $attributes = \array_merge([], ...$attributes);
-
-        return \array_map(static fn (\ReflectionAttribute $attribute) => $attribute?->getName(), $attributes);
+        return false;
     }
 }
